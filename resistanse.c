ca008fb716960b0f9f2845c842dc3613f466b9ec
T0g1r <urazbahtintalgat@gmail.com>
2025-09-12 19:00:47 +0300

    Add files via upload

---
+++ resistanse.c
@@ -0,0 +1,30 @@
+#include "resistanse.h"
+
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+
+int data_select(int M, float resistance[]) {
+    assert(M > 0);
+    assert(resistance);
+
+    int K = M;
+    float average = 0;
+    for (int i = 0; i < M; i++) {
+        average += resistance[i];
+    }
+    average /= M;
+    for (int i = 0; i < M; i++) {
+        if (fabs(resistance[i] - average) / average > 0.03){
+            K--;
+            resistance[i] = -1;
+        }
+    }
+    for (int i = 0; i < M - 1; i++){
+        if (resistance[i] == -1){
+            resistance[i] = resistance[i+1];
+            resistance[i+1] = -1;
+        }
+    }
+    return K;
+}
