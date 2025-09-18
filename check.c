0074266f43abe9a86e9e6988a073c528cfe583d9
T0g1r <urazbahtintalgat@gmail.com>
2025-09-18 18:09:26 +0300

    fix

--- check.c
+++ check.c
@@ -9,7 +9,7 @@ int check(int n, float* resistance, float resistance_final)
 
     float sum = 0;
     for (int i = 0; i < n; ++i) {
-        sum += resistance[i];
+        sum += fabs(resistance[i] - resistance_final);
     }
-    return fabsf(sum / n) < EPSILON; // Так как число с плавающей точкой
+    return fabsf(sum / n) < 1e-2; // Так как число с плавающей точкой
 }
--- resistanse.c
+++ resistanse.c
@@ -10,24 +10,20 @@ int data_select(int M, float resistance[]) {
     assert(M > 0);
     assert(resistance);
 
-    int K = M;
+    //int K = M;
     float average = 0;
     for (int i = 0; i < M; i++) {
         average += resistance[i];
     }
     average /= M;
+    float * a = resistance;
     for (int i = 0; i < M; i++) {
     // TODO: 0.03 лучше вынести в переменную, например, константную
         if (fabs(resistance[i] - average) / average > 0.03){
-            K--;
             resistance[i] = -1;
+        } else {
+            *a++ = resistance[i];
         }
     }
-    for (int i = 0; i < M - 1; i++){
-        if (resistance[i] == -1){
-            resistance[i] = resistance[i+1];
-            resistance[i+1] = -1;
-        }
-    }
-    return K;
+    return a - resistance;
 }
